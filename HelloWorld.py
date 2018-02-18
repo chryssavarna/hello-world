4fc6a206b598e1d479786159422af3a0998f09e6
chryssavarna <36103536+chryssavarna@users.noreply.github.com>
2018-02-18 16:31:32 +0000

    Write Hello World
    Ask for User Input

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+#Print an introduction
+print("Welcome to Python")
+#Ask for input
+
+text = input("Would you like to print Hello World? Y(yes) or N(No)")
+#Check your status
+if text == "Y":
+    print("Hello World")
+else:
+    pass
